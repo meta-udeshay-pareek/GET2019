dd49074ee13243b07ebd92763750699248803c23
Udeshay Pareek <udeshay987@gmail.com>
2019-01-17 17:09:48 +0530

    first commit for Assignment2_Part1

--- Assignment2_Part1/ConvertNumber.java
+++
@@ -1,9 +0,0 @@
-package hexcalc;
-
-public interface ConvertNumber {
-	//Will convert number into decimal
-	int stringToNumber(String num);
-	
-	//will convert any base number to decimal
-	String numberToString(int num);
-}
