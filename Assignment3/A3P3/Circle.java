e74b27a072c690ee9a828fbcb8e0debe85d29451
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-27 18:06:38 +0530

    Update Circle.java

--- Assignment3/A3P3/Circle.java
+++ Assignment3/A3P3/Circle.java
@@ -5,19 +5,19 @@ public class Circle extends Shape {
     private final double radius;
     final double pi = Math.PI;
 
-    public Circle(double radius) {
-        this.radius = radius;
+    public Circle() {
+         radiousInput();
     }
 
-    private double radiousInput(){
+    private void radiousInput(){
 	    Scanner sc = new Scanner(System.in);
 	    System.out.println("Enter Radious :");
-	    int r = sc.nextDouble();
-	    return r;
+	    this.radius = sc.nextDouble();
+	   
     }
     @Override
     public double area() {
         // A = π r^2
-        return pi * Math.pow(radiousInput(), 2);
+        return pi * Math.pow(this.radius, 2);
     }
 }
