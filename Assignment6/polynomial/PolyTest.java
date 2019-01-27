6e520484b167b11891a1074de587e17b8d321204
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-27 17:28:25 +0530

    Update PolyTest.java

--- Assignment6/polynomial/PolyTest.java
+++ Assignment6/polynomial/PolyTest.java
@@ -1,52 +1,66 @@
 import static org.junit.Assert.*;
 
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-
 public class PolyTest {
-    private Poly poly;
-    private static final int pow[] = {2,1,0};
-    private static final int coeff[] = {3,2,4};
 
-    @Test
-    public void evaluateTest() {
-        poly = new Poly(pow,coeff);
-        assertEquals(20.0,poly.evaluate(2.0f),0);
-    }
-    
-    @Test
-    public void degreeTest() {
-        poly = new Poly(pow,coeff);
-        assertEquals(2,poly.degree());
-    }
-    
-    @Test
+	
+	@Test
+	public void evaluateTest() {
+		int[] arr=new int[]{2,0};
+		int[] arr1=new int[]{2,1};
+		Poly p1 = new Poly(arr,arr1);
+		assertEquals(9.0,p1.evaluate(2.0F),0.001);
+		
+	}
+	@Test
+	public void degreeTest() {
+		int[] arr=new int[]{2,1,0};
+		int[] arr1=new int[]{3,2,1};
+		Poly p1 = new Poly(arr,arr1);
+		assertEquals(2,p1.degreeOfPoly());
+		
+	}
+	@Test
     public void addPolyTest() {
-        poly = new Poly();
-        int pow1[] = {2,1,0};
-        int coeff1[] ={3,2,4};
-        Poly poly1 = new Poly(pow1,coeff1);
-        int pow2[] = {1,2,3,4};
-        int coeff2[] ={4,1,2,3};
-        Poly poly2 = new Poly(pow2,coeff2);
-        int result_pow[] = {4,3,2,1,0};
-        int result_coeff[] ={3,2,4,6,4};
-        assertArrayEquals(result_pow,poly.addPoly(poly1,poly2).getPower());
-        assertArrayEquals(result_coeff,poly.addPoly(poly1,poly2).getCoeff());
+        int[] arr=new int[]{2,1,0};
+        int[] arr1=new int[]{3,2,1};
+        int[] arr2=new int[]{1,0};
+        int[] arr3=new int[]{4,5};
+        int[][] arrOut=new int[][] {{2,3},{1,6},{0,6}};
+        Poly p1 = new Poly(arr,arr1);
+        Poly p2 = new Poly(arr2,arr3);
+        Poly p3 = new Poly(new int[] {},new int[] {});
+        assertArrayEquals(arrOut,p3.addPoly(p1, p2));
+        
     }
-    
-    @Test
-    public void multiplyPolyTest() {
-        poly = new Poly();
-        int pow1[] = {2,1,0};
-        int coeff1[] ={3,2,4};
-        Poly poly1 = new Poly(pow1,coeff1);
-        int pow2[] = {2,4};
-        int coeff2[] ={1,3};
-        Poly poly2 = new Poly(pow2,coeff2);
-        int result_pow[] = {6,5,4,3,2};
-        int result_coeff[] ={9,6,15,2,4};
-        assertArrayEquals(result_pow,poly.multiplyPoly(poly1,poly2).getPower());
-        assertArrayEquals(result_coeff,poly.multiplyPoly(poly1,poly2).getCoeff());
+	@Test
+    public void multipilyPolyTest() {
+        int[] arr=new int[]{2,1,0};
+        int[] arr1=new int[]{3,2,1};
+        int[] arr2=new int[]{1,0};
+        int[] arr3=new int[]{4,5};
+        int[][] arrOut=new int[][] {{3,12},{2,23},{1,14},{0,5}};
+        Poly p1 = new Poly(arr,arr1);
+        Poly p2 = new Poly(arr2,arr3);
+        Poly p3 = new Poly(new int[] {0},new int[] {0});
+        assertArrayEquals(arrOut,p3.multiplyPoly(p1, p2));
+        
+    }
+	@Test
+    public void multipilyPolyTest1() {
+        int[] arr=new int[]{4,1,0};
+        int[] arr1=new int[]{5,3,2};
+        int[] arr2=new int[]{2,0};
+        int[] arr3=new int[]{7,3};
+        int[][] arrOut=new int[][] {{6,35},{4,15},{3,21},{2,14},{1,9},{0,6}};
+        Poly p1 = new Poly(arr,arr1);
+        Poly p2 = new Poly(arr2,arr3);
+        Poly p3 = new Poly(new int[] {0},new int[] {0});
+        assertArrayEquals(arrOut,p3.multiplyPoly(p1, p2));
+        
     }
 }
