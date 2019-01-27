bc12941127acd8cd76f6c1df8697a086a85603e6
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-27 21:52:36 +0530

    Update Circle.java

--- Assignment3/A3P3/Circle.java
+++ Assignment3/A3P3/Circle.java
@@ -1,8 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package area;
+
 import java.util.*;  
 
 public class Circle implements Shape {
-    private final double radius;
+    private  double radius;
     final double pi = Math.PI;
 
     public Circle() {
