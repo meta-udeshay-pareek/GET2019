8881d97e2a94830e69c1616d849b37ec0b5b8e5c
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-27 16:34:34 +0530

    Update HexConversion.java

--- Assignment2P1/HexConversion.java
+++ Assignment2P1/HexConversion.java
@@ -3,13 +3,13 @@ package hexcalc;
 public class HexConversion implements ConvertNumber {
 	
 	//converting hex to decimal
-	public int stringToNumber(String hexNum){
+	public static int stringToNumber(String hexNum){
 		
 		return Integer.parseInt(hexNum,16);
 	}
 	
 	//converting decimal to hexadecimal
-	public String numberToString(int num){
+	public static String numberToString(int num){
 		return Integer.toHexString(num);
 	}
 	
