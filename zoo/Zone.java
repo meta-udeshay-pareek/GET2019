e2ad103af5e61e858962256067534a29dd127182
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-29 12:22:54 +0530

    Update Zone.java

--- zoo/Zone.java
+++ zoo/Zone.java
@@ -1,13 +1,15 @@
 package zoo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Zone {
 	
+	
 	int capacity;//number of cage capacity in zone
 	boolean hasCanteen,hasPark;//true and false respectively if exist or not exist
 	
-	Animal animlalCategoryType;//category of animal in this zone like Mammal,Reptile,Bird etc
+	String animlalCategoryType;//category of animal in this zone like Mammal,Reptile,Bird etc
 	
 	List<Cage> cages;//List of cages ,one zone can contain multiple number of cages
 	
@@ -19,21 +21,25 @@ public class Zone {
 	 * @param hasCanteen, true if has else false
 	 * @param hasPark, true if has else false
 	 * */
-	Zone(int capacity,Animal animalCategoryType,boolean hasCanteen,boolean hasPark){
+	public Zone(int capacity,String animalCategoryType,boolean hasCanteen,boolean hasPark){
 		this.capacity = capacity;
-		this.animlalCategoryType = animalCategoryType;
+		this.animlalCategoryType = animalCategoryType.toUpperCase();//assigning after converting string into uppercase
 		this.hasCanteen = hasCanteen;
 		this.hasPark = hasPark;
+		this.cages = new ArrayList<Cage>();
 	}
 	
 	
-	
-	void addCage(Cage cage){
+	/*
+	 * @param cage, the cage which is going to be add in this zone
+	 * @return "true" if successfully added else "false"
+	 * */
+	boolean addCage(Cage cage){
 		
 		//Zone animal category class and super class of cage animal type must be same 
 		//e.g. If zone is of Mammal type and cage animal type is Lion then it is OK
 		//but if cage animal type is Peacock then it shouldn't be added 
-		if(this.animlalCategoryType.getClass()==cage.animalType.getClass().getSuperclass()){
+		if(this.animlalCategoryType.equals(cage.getAnimalCategory())){
 			
 			//if number of cages are in under of capacity of zone then cage will be added in this zone
 			if(this.cages.size()<this.capacity){
@@ -41,12 +47,15 @@ public class Zone {
 				this.capacity++;
 			}else{
 				System.out.println("This Zone is out of capacity");
+				return false;
 			}
 		}
 		else{
 			System.out.println("this cage animal type are not under in this zone category element");
+			return false;
 		}
 		
+		return true;
 	}
 	
 	/*
