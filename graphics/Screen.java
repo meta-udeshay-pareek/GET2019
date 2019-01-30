70e4b6df03f9f630a3839fbb01108289ad888098
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-30 19:33:34 +0530

    Update Screen.java

--- graphics/Screen.java
+++ graphics/Screen.java
@@ -3,10 +3,25 @@ package graphics;
 import graphics.Shape.ShapeType;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Screen {
+	
+	private TreeMap<Date, Shape> shapeWithTime ;
+	private TreeMap<Shape, Double> shapeWithArea;
+	private TreeMap<Double, Shape> shapeWithPerimeter;
+	private TreeMap<Double, Shape> shapeWithOriginDistance;
+	
+	Screen(){
+		shapeWithTime = new TreeMap<Date, Shape>();
+		shapeWithArea = new TreeMap<Shape, Double>();
+		shapeWithPerimeter = new TreeMap<Double, Shape>();
+		shapeWithOriginDistance = new TreeMap<Double, Shape>();
+	}
 	
 	//List of ScreenElements on the screen
     private List <ScreenElement> screenElements= new ArrayList<ScreenElement>();
@@ -27,6 +42,15 @@ public class Screen {
      */
     public void addShape(Shape shape){
         screenElements.add(new ScreenElement(shape));
+        
+		this.shapeWithTime.put(new Date(), shape);
+		this.shapeWithArea.put(shape, shape.getArea());
+		this.shapeWithPerimeter.put(shape.getPerimeter(), shape);
+		try {
+			this.shapeWithOriginDistance.put(shape.getShapeOrigin().distanceFromPoint(new Point(0, 0)), shape);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
     }
     
     
@@ -67,37 +91,67 @@ public class Screen {
     }
     
     
-    /*
-     * @return  shapeNames in ascending order based on area
-     * */
-    public String[] displayinAscending() throws Exception {
-
-        String shapeNames[] = new String[screenElements.size()];
-
-        for (int i = 0; i < shapeNames.length; i++) {
-            shapeNames[i] = screenElements.get(i).getShape().getType();
-        }
-        checkForEmptyAndThrowException();
-
-        int i = 0;
-        while (i < screenElements.size()) {
-            int j = 0;
-            while (j < (screenElements.size() - 1) - i) {
-
-                if (screenElements.get(j).getShape().getArea() > screenElements.get(j + 1).getShape().getArea()) {
-
-                    String temporary = shapeNames[j];
-                    shapeNames[j] = shapeNames[j + 1];
-                    shapeNames[j + 1] = temporary;
-
-                }
-                j++;
-            }
-            i++;
-        }
-
-        return shapeNames;
-    }
+    /**
+	 * 
+	 * @return - List of Shapes in sorted according to timestamp
+	 */
+	public List<Shape> sortShapeWithTimestamp(){
+		List<Shape> list = new ArrayList<Shape>();
+		for (Map.Entry<Date, Shape> entry : this.shapeWithTime.entrySet()) {
+	        list.add(entry.getValue());
+	    }
+		return list;
+	}
+	
+	/**
+	 * 
+	 * @return - List of Shapes in sorted according to Area
+	 */
+	public List<Shape> sortShapeWithArea(){
+		List<Shape> list = new ArrayList<Shape>();
+		for (Map.Entry<Shape, Double> entry : this.shapeWithArea.entrySet()) {
+	        list.add(entry.getKey());
+	    }
+		return list;
+	}
+	
+	/**
+	 * 
+	 * @return - List of Shapes in sorted according to Perimeter
+	 */
+	public List<Shape> sortShapeWithPerimeter(){
+		List<Shape> list = new ArrayList<Shape>();
+		for (Map.Entry<Double, Shape> entry : this.shapeWithPerimeter.entrySet()) {
+	        list.add(entry.getValue());
+	    }
+		return list;
+	}
+	
+	/**
+	 * 
+	 * @return - List of Shapes in sorted according to Origin Distance
+	 */
+	public List<Shape> sortShapeWithOriginDistance(){
+		List<Shape> list = new ArrayList<Shape>();
+		for (Map.Entry<Double, Shape> entry : this.shapeWithOriginDistance.entrySet()) {
+	        list.add(entry.getValue());
+	    }
+		return list;
+	}
+	
+	/**
+	 * 
+	 * @return - List of Shapes in given Point is Enclosed
+	 */
+	public List<Shape> getShapesInPointIsEnclosed(Point point){
+		List<Shape> list = new ArrayList<Shape>();
+		for (Map.Entry<Shape, Double> entry : this.shapeWithArea.entrySet()) {
+	        if(entry.getKey().isPointEnclosed(point)) {
+	        	list.add(entry.getKey());
+	        }
+	    }
+		return list;
+	}
     
     
     
