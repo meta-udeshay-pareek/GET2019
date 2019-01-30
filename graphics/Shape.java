0e5dc01e8815bc1acb9b57bd7b7bbc480b3784dd
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-30 18:49:15 +0530

    Update Shape.java

--- graphics/Shape.java
+++ graphics/Shape.java
@@ -1,12 +1,12 @@
 package graphics;
 
 public interface Shape {
-	public double getArea();
-    public double getPerimeter();
-    public Point getShapeOrigin();
-    public boolean isPointEnclosed(Point p);
-    public double getDistanceFromOrigin();
-    public String getType();
+    double getArea();
+    double getPerimeter();
+    Point getShapeOrigin();
+    boolean isPointEnclosed(Point p);
+    double getDistanceFromOrigin();
+    String getType();
     
     enum ShapeType{
         SQUARE,RECTANGLE,CIRCLE,TRIANGLE
