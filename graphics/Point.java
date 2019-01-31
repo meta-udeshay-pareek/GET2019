9c5ca0c109e23805966d3b3527b0db3f4184516e
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-01-31 09:05:08 +0530

    Update Point.java

--- graphics/Point.java
+++ graphics/Point.java
@@ -1,20 +1,53 @@
 package graphics;
 
-public class Point {
+	private int x;
+	private int y;
 	
-	private int x,y;//co-ordinates
-    
-    public Point(int x,int y){
-        this.x= x;
-        this.y=y;
-    }
-    
-    public int getX(){
-        return this.x;
-    }
-    
-    public int getY(){
-        return this.y;
-    }
-
+	
+	Point(int x, int y) throws InvalidCoordinate{
+		if( x < 0 || y < 0) {
+			throw new InvalidCoordinate("Coordinates exceed screen size!");
+		}
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Custom Exception class 
+	 * @author User29
+	 *
+	 */
+	class InvalidCoordinate extends Exception{
+		
+		InvalidCoordinate(String exception){
+			
+			super(exception);
+		}
+	}
+	
+	/**
+	 * 
+	 * @return - X coordinate of Point
+	 */
+	public int getX() {
+		return this.x;
+	}
+	
+	/**
+	 * 
+	 * @return - Y coordinate of Point
+	 */
+	public int getY() {
+		return this.y;
+	}
+	
+	/**
+	 * Calculate distance between Both points
+	 * @param point - Second point
+	 * @return - Distance between both point
+	 */
+	public double distanceFromPoint(Point point) {
+		
+		return Math.sqrt(Math.pow((point.getX() - this.getX()), 2) + Math.pow(( point.getY() - this.getY() ), 2));
+	}
 }
