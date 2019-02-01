c9224e0c3446bab766bfe1b74f1dbe7806a3a3c8
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-02-01 18:44:47 +0530

    Update Triangle.java

--- graphics/Triangle.java
+++ graphics/Triangle.java
@@ -1,13 +1,13 @@
 package graphics;
 
-class Triangle extends ShapeDesign{
+class Triangle implements Shape{
 	
 	private int base;
 	private int height;
-   
+	private Point originPoint;
     
     public Triangle(Point p,int base, int height){
-        this.p = p;
+        this.originPoint = p;
         this.base = base;
         this.height = height;
       
@@ -66,31 +66,69 @@ class Triangle extends ShapeDesign{
     */
     @Override
     public Point getShapeOrigin(){
-        return this.p;
+        return this.originPoint;
     }
     
-    
     /*
+    * |\
+    * | \
+    * |  \
+    * |   \
+    * |    \
+    * |     \
+    * |      \
+    * |       \  
+    * |________\
+    * 
+    *  1.creating three triangle inside original triangle by jointing all the three point with inner point which is given by user
+    *  2.calculating area of all three triangle ,if addition of area of those three triangle is equal to the area of original triangle
+    *   then point is enclosed else not.
+    *  
     *@param p , point p(x,y) 
     *@return "true" if exist in shape area else "false"
     */
     @Override
-    public boolean isPointEnclosed(Point point){
-    	try {
-			Point p1 = new Point(this.p.getX()+this.base, this.p.getY());
-			Point p2 = new Point(this.p.getX(), this.p.getY()+this.height);
-			if(point.getX()>=this.p.getX() && point.getY() >= this.p.getY()) {
-				double left = point.getY()*( p2.getX() - p1.getX() ) - point.getX()*(p2.getY() - p1.getY());
-				double right = p1.getY()*p2.getX() - p1.getX()*p2.getY();
-				if(left >= right) {
-					return true;
-				}
-			}
-		}catch(Exception e) {
-			System.out.println(e);
-		}
-		return false;
+    public boolean isPointEnclosed(Point p) {
+    	
+        double x1=this.originPoint.getX();
+        double y1=this.originPoint.getY()+this.getHeight();
+        double x2=this.originPoint.getX()+ this.getBase();
+        double y2=this.originPoint.getY();
+        
+        double hypotenuse=Math.sqrt(Math.pow(height,2)+Math.pow(base,2));
+        double side = Math.sqrt(Math.pow((p.getX()-x1),2)+Math.pow((p.getY()-y1),2));
+        double side1 = Math.sqrt(Math.pow((p.getX()-this.originPoint.getX()),2)+Math.pow((p.getY()-this.originPoint.getY()),2));
+        double side2= Math.sqrt(Math.pow((p.getX()-x2),2)+Math.pow((p.getY()-y2),2));
+        
+        double peri= (side+side1+height)/2;
+        double peri1= (side1+side2+base)/2;
+        double peri2= (side+side2+hypotenuse)/2;
+        
+        double area=getArea();
+        double area1=Math.sqrt(peri*(peri-side)*(peri-height)*(peri-side1));
+        double area2=Math.sqrt(peri1*(peri-side1)*(peri1-base)*(peri1-side2));
+        double area3=Math.sqrt(peri2*(peri-side)*(peri1-hypotenuse)*(peri2-side2));
+        if((area1+area2+area3)==area)
+            return true;
+        else
+            return false;
     }
+
+
+    /*
+     *@return distance,from origin point to shape origin point
+     */
+	@Override
+	public double getDistanceFromOrigin() {
+		double distance=0 ;
+		try{
+			distance= this.originPoint.distanceFromPoint(new Point(0,0));
+		}catch(Exception e){
+			System.out.print(e);
+		}
+		
+		return distance;
+	}
     
     
     
