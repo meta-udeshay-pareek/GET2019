851d4b01b9655c36f692ec67e89f5feb9bea9885
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-02-01 18:16:50 +0530

    Update Point.java

--- graphics/Point.java
+++ graphics/Point.java
@@ -1,19 +1,40 @@
 package graphics;
 
-public class Point{
-	private int x;
-	private int y;
+public class Point {
 	
+	private int x,y;//co-ordinates
+    
+	private final int XMAX = 1000;
+	private final int YMAX = 1000;
 	
 	Point(int x, int y) throws InvalidCoordinate{
-		if( x < 0 || y < 0) {
+		if(x > XMAX || y > YMAX || x < 0 || y < 0) {
 			throw new InvalidCoordinate("Coordinates exceed screen size!");
 		}
 		this.x = x;
 		this.y = y;
 	}
-	
-	/**
+    
+    public int getX(){
+        return this.x;
+    }
+    
+    public int getY(){
+        return this.y;
+    }
+
+    
+    /**
+	 * Calculate distance between Both points
+	 * @param point - Second point
+	 * @return - Distance between both point
+	 */
+	public double distanceFromPoint(Point point) {
+		
+		return Math.sqrt(Math.pow((point.getX() - this.getX()), 2) + Math.pow(( point.getY() - this.getY() ), 2));
+	}
+    
+    /**
 	 * Custom Exception class 
 	 * @author User29
 	 *
@@ -26,29 +47,6 @@ public class Point{
 		}
 	}
 	
-	/**
-	 * 
-	 * @return - X coordinate of Point
-	 */
-	public int getX() {
-		return this.x;
-	}
 	
-	/**
-	 * 
-	 * @return - Y coordinate of Point
-	 */
-	public int getY() {
-		return this.y;
-	}
 	
-	/**
-	 * Calculate distance between Both points
-	 * @param point - Second point
-	 * @return - Distance between both point
-	 */
-	public double distanceFromPoint(Point point) {
-		
-		return Math.sqrt(Math.pow((point.getX() - this.getX()), 2) + Math.pow(( point.getY() - this.getY() ), 2));
-	}
 }
