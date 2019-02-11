2dc7a2a6e65f00e6c7e03e6f47bdc2b9436db0e8
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-02-11 13:06:57 +0530

    Update Crocodile.java

--- zoo/Crocodile.java
+++ zoo/Crocodile.java
@@ -5,10 +5,7 @@ public class Crocodile extends Reptile {
     public Crocodile(){}
 
 	public Crocodile(String name,double age,double weight){
-		this.name= name;
-		this.age = age;
-		this.weight = weight;
-		this.animalId = getUniqueId();//static method of Animal abstract class which will generate unique id 
+		super(name,age,weight);
 	}
 	
 	@Override
@@ -16,13 +13,7 @@ public class Crocodile extends Reptile {
 		return "hiss";
 	}
 	
-	/*
-	 * @return animalId,unique id of animal
-	 * */
-	public int getAnimalId(){
-		return this.animalId;
-	}
-      
+
 	/*
 	* @return AnimalType animal
 	* */
@@ -30,17 +21,5 @@ public class Crocodile extends Reptile {
 	     return "CROCODILE";
 	}
       
-	/*
-	* @return "true" if any of the information of animal is null or 0 else return "false"
-	* */
-	public boolean isAnimalInfoNull(){
-		if(this.name==null){
-	  		return true;
-	  	}else if (this.age==0){
-	  		return true;
-	  	}else if (this.weight==0){
-	  		return true;
-	  	}
-	  	return false;
-	}
+	
 }
