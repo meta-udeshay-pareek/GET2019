5c8243562162e98ece42d5ad5a15c4e52d4276f8
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-02-11 13:08:37 +0530

    Update Peacock.java

--- zoo/Peacock.java
+++ zoo/Peacock.java
@@ -1,46 +1,23 @@
 package zoo;
 
 public class Peacock extends Bird {
+	
+    public Peacock(){}
 
-	public Peacock(){}
-
-	public Peacock(String name,double age,double weight){
-		this.name = name;
-		this.age = age;
-		this.weight= weight;
-		this.animalId = getUniqueId();//static method of Animal abstract class which will generate unique id 
-	}
+    public Peacock(String name,double age,double weight){
+    	super(name,age,weight);
+    }
 
 	@Override
 	public String getSound(){
 		return "scream";
 	}
-
-	/*
-	 * @return animalId,unique id of animal
-	 * */
-	public int getAnimalId(){
-		return this.animalId;
-	}
-
-	/*
+    
+     /*
 	 * @return AnimalType animal
 	 * */
-	public String getAnimalType(){
-		return "PEACOCK";
-	}
-
-	/*
-	* @return "true" if any of the information of animal is null or 0 else return "false"
-	* */
-	public boolean isAnimalInfoNull(){
-		if(this.name==null){
-			return true;
-		}else if (this.age==0){
-			return true;
-		}else if (this.weight==0){
-			return true;
-		}
-	   return false;
-	}
+    public String getAnimalType(){
+        return this.getClass().getSimpleName();
+    }
+    
 }
