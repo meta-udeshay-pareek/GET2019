439208e2fa8bdd4081669e183078d30cd3f07654
Udeshay Pareek <udeshay987@gmail.com>
2019-02-26 19:38:03 +0530

    SpringMvc

---
+++ SpringMVC/src/main/java/com/metacube/training/service/UserService.java
@@ -0,0 +1,18 @@
+package com.metacube.training.service;
+
+import java.util.List;
+
+import com.metacube.training.model.User;
+
+public interface UserService {
+	
+	public boolean add(User user);
+	
+	public User get(Integer id);
+	
+	public boolean update(User user);
+	
+	public List<User> getAll();
+	
+	public boolean delete(Integer id);
+}
