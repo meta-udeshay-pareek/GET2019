5ddbfc7687278589d33fe3a7d8631253acb72e33
Udeshay Pareek <udeshay987@gmail.com>
2019-02-27 17:46:47 +0530

    Updated

--- src/services/MessageService.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-public interface MessageService {
-	/*
-	 * @param message which is to be send 
-	 * @param receiver ,to whom message is going to send
-	 * */
-	public void sendMessage(String message,String receiver);
-}
