13df8c12d1f128ee9c16cf03a0b998a73f773dbe
Udeshay Pareek <udeshay987@gmail.com>
2019-02-28 20:04:06 +0530

    EAD/Assignment-6

---
+++ SpringBootMvc/src/main/java/com/metacube/training/model/Admin.java
@@ -0,0 +1,37 @@
+package com.metacube.training.model;
+
+public class Admin {
+	
+	int id;
+
+	String userName;
+	String password;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return userName;
+	}
+	
+	public void setUsername(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
