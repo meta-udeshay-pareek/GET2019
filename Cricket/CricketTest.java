5da1aa759d3e0afeea104ce1b04e80492ce5a6b6
meta-udeshay-pareek <46710014+meta-udeshay-pareek@users.noreply.github.com>
2019-03-15 15:05:44 +0530

    Update CricketTest.java

--- Cricket/CricketTest.java
+++ Cricket/CricketTest.java
@@ -6,14 +6,18 @@ public class CricketTest {
 
 	@Test
 	public void testfindOrderOfBolres() throws Exception {
-		assertArrayEquals(new int[] {2,3,4,1}, Cricket.findOrderOfBolres(13, 4, new int[] {1,6,3,3}));
-		assertArrayEquals(new int[] {2,1,3}, Cricket.findOrderOfBolres(5, 3, new int[] {1,3,1}));
-		assertArrayEquals(new int[] {2,1,3}, Cricket.findOrderOfBolres(7, 3, new int[] {2,4,1}));
-		assertArrayEquals(new int[] {1,3,2,4}, Cricket.findOrderOfBolres(9, 4, new int[] {3,2,3,1}));
-		assertArrayEquals(new int[] {2,3,7,1,4,5,6}, Cricket.findOrderOfBolres(11, 7, new int[] {1,3,2,1,1,1,2}));
-		assertArrayEquals(new int[] {2,1}, Cricket.findOrderOfBolres(5, 2, new int[] {2,3}));
-		assertArrayEquals(new int[] {4,2,3,1}, Cricket.findOrderOfBolres(23, 4, new int[] {1,8,5,9}));
-		assertArrayEquals(new int[] {1,4,2,3}, Cricket.findOrderOfBolres(21, 4, new int[] {10,3,2,6}));
+		System.out.println(Cricket.findOrderOfBolres(5, 2, new int[] {3,2}));
+		for(int x :Cricket.findOrderOfBolres(5, 2, new int[] {3,2})) {
+			System.out.println(x);
+		}
+		assertArrayEquals(new int[] {1,1,2,1,2}, Cricket.findOrderOfBolres(5, 2, new int[] {3,2}));
+		assertArrayEquals(new int[] {2,2,1,2,3}, Cricket.findOrderOfBolres(5, 3, new int[] {1,3,1}));
+		assertArrayEquals(new int[] {2,2,1,2,1,2,3}, Cricket.findOrderOfBolres(7, 3, new int[] {2,4,1}));
+		assertArrayEquals(new int[] {1,3,1,2,3,1,2,3,4}, Cricket.findOrderOfBolres(9, 4, new int[] {3,2,3,1}));
+		assertArrayEquals(new int[] {2,2,3,7,1,2,3,4,5,6,7}, Cricket.findOrderOfBolres(11, 7, new int[] {1,3,2,1,1,1,2}));
+		assertArrayEquals(new int[] {2,1,2,1,2}, Cricket.findOrderOfBolres(5, 2, new int[] {2,3}));
+		assertArrayEquals(new int[] {1,1,3,1,3,1,2,3}, Cricket.findOrderOfBolres(8, 3, new int[] {4,1,3}));
+		
 
 	}
 
