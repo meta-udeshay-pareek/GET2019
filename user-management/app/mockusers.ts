eb0dd76c12507af85d6e9c7ad89b4001129c35f2
Udeshay Pareek <udeshay987@gmail.com>
2019-03-20 14:37:40 +0530

    EAD/Assignment-9

---
+++ user-management/app/mockusers.ts
@@ -0,0 +1,12 @@
+//this file contains the list of users which will be displayed on the page
+
+import { user } from './user';
+
+export const users: user[]=[
+    {name: "Akshat", contact: 7788994455, city:"Jaipur"},
+    {name: "Shubham", contact: 7788994455, city:"Mumbai"},
+    {name: "Rahul", contact: 7788994455, city:"Delhi"},
+    {name: "Nitesh", contact: 7788994455, city:"Jaipur"},
+    {name: "Joy", contact: 7788994455, city:"Ahmedabad"},
+    {name: "Jaya", contact: 7788994455, city:"Kolkata"},
+];
---
+++ user-management/app/user.ts
@@ -0,0 +1,6 @@
+//this is the model class of user which contains the following properties
+export class user{
+    name: String;
+    contact: number;
+    city: String;
+}
